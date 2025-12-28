c4bc3cd7712bd697c42a06dd5581d10417f41f6e
lawirit <liljafatouma8473@hotmail.com>
2025-12-28 19:18:28 +0100

    Add master mapping file for bird name variants

---
+++ name_mapping.py
@@ -0,0 +1,43 @@
+# Master mapping of bird name variants to standardized names
+# Used by standardizer.py to normalize inconsistent bird names
+
+BIRD_NAME_MAPPING = {
+    # American Robin variants
+    "American Robin": "American Robin",
+    "Robin, American": "American Robin",
+    "Robin": "American Robin",
+    
+    # Blue Jay variants
+    "Blue Jay": "Blue Jay",
+    
+    # American Crow variants
+    "C. Crow": "American Crow",
+    "American Crow": "American Crow",
+    
+    # Northern Cardinal variants
+    "Northern Cardinal": "Northern Cardinal",
+    "Cardinal, Northern": "Northern Cardinal",
+    "N. Cardinal": "Northern Cardinal",
+    
+    # Mallard variants
+    "Mallard Duck": "Mallard",
+    "Mallard": "Mallard",
+    
+    # Red-tailed Hawk variants
+    "Red-tailed Hawk": "Red-tailed Hawk",
+    "Red tailed Hawk": "Red-tailed Hawk",
+    "Red-tailed": "Red-tailed Hawk",
+    
+    # House Sparrow variants
+    "House Sparrow": "House Sparrow",
+    "House Sparrow (English)": "House Sparrow",
+    
+    # American Goldfinch variants
+    "American Goldfinch": "American Goldfinch",
+    "Goldfinch, American": "American Goldfinch",
+    
+    # Great Blue Heron variants
+    "Great Blue Heron": "Great Blue Heron",
+    "G. B. Heron": "Great Blue Heron",
+    "Great Blue": "Great Blue Heron",
+}
